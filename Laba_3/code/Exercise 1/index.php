841f1ad86309b8f8bba8c0bcadf50be374f98594
GrigorevaAnna <72629044+GrigorevaAnna@users.noreply.github.com>
2024-03-29 14:48:46 +0200

    Regular expressions_b

--- Laba_3/code/Exercise 1/index.php
+++ Laba_3/code/Exercise 1/index.php
@@ -9,3 +9,13 @@ foreach($matches[0] as $word)
     echo $word;
     echo "\n";
 }
+
+/*--------Regular expressions_b--------*/
+$text = 'a1b2c3';
+$regexp = '/[0-9]/u';
+function degree($matches)
+{
+    return pow($matches[0], 3);
+}
+$result = preg_replace_callback($regexp, 'degree', $text);
+echo "Результат: {$result}\n";
