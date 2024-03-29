00ac4f14ce8116013edd5164ebb98055916f3a16
GrigorevaAnna <72629044+GrigorevaAnna@users.noreply.github.com>
2024-03-29 14:48:23 +0200

    Regular expressions_a

--- Laba_3/code/Exercise 1/index.php
+++ Laba_3/code/Exercise 1/index.php
@@ -1,1 +1,11 @@
 <?php
+/*--------Regular expressions_a--------*/
+$text = 'ahb acb aeb aeeb adcb axeb';
+$regexp = "/a[a-z][a-z]b/ui";
+$result = preg_match_all($regexp, $text, $matches);
+echo "Найдено слов: {$result}\n";
+foreach($matches[0] as $word)
+{
+    echo $word;
+    echo "\n";
+}
