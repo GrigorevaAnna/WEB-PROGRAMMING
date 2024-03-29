24014c0857cd5caaff2c22631fc5666b50f22d9d
GrigorevaAnna <72629044+GrigorevaAnna@users.noreply.github.com>
2024-03-29 21:41:49 +0200

    we output the elements

---
+++ Laba_3/code/Exercise_2c.2.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+echo "<ul>";
+if (isset($_SESSION['data']))
+{
+    echo "<ul>";
+    foreach ($_SESSION['data'] as $key => $value)
+    {
+        echo "<li>$key: $value</li>";
+    }
+    echo '</ul>';
+}
+echo "<ul>";
+?>
