e5fcfb9037358d1cd94bd733c86f55cb94da3749
GrigorevaAnna <72629044+GrigorevaAnna@users.noreply.github.com>
2024-05-03 12:51:23 +0200

    getting and adding data to a table

--- Laba_4/code/save.php
+++ Laba_4/code/save.php
@@ -8,4 +8,18 @@ $client->addScope(Google_Service_Sheets::SPREADSHEETS);
 $service = new Google_Service_Sheets($client);
 
 $spreadsheetId = '1chZy_qQ4PDAmxNpDHyqNcGb0kccKXxAr378czZyi4rc';
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $email = $_POST['email'];
+    $category = $_POST['category'];
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+
+    $row = [$email, $category, $title, $description];
+
+    $range = 'Лист1!A1';
+    $valueRange = new Google_Service_Sheets_ValueRange(['values' => [$row]]);
+    $service->spreadsheets_values->append($spreadsheetId, $range, $valueRange, ['valueInputOption' => 'USER_ENTERED']);
+
 ?>
