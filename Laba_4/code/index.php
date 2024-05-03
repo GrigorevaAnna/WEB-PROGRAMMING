ebb0e6734e3baae4fdc2668e77752c9e6a680a66
GrigorevaAnna <72629044+GrigorevaAnna@users.noreply.github.com>
2024-05-03 12:46:31 +0200

    the part with ads

--- Laba_4/code/index.php
+++ Laba_4/code/index.php
@@ -38,6 +38,31 @@
         <th>Description</th>
         </thead>
         <tbody>
+        <?php
+        require_once 'vendor/autoload.php';
+
+        $client = new Google_Client();
+        $client->setAuthConfig(__DIR__.'/credentials.json');
+        $client->addScope(Google_Service_Sheets::SPREADSHEETS);
+
+        $service = new Google_Service_Sheets($client);
+
+        $spreadsheetId = '1chZy_qQ4PDAmxNpDHyqNcGb0kccKXxAr378czZyi4rc';
+
+        $response = $service->spreadsheets_values->get($spreadsheetId, 'Лист1');
+        $values = $response->getValues();
+
+        if (!empty($values)) {
+            foreach ($values as $row) {
+                echo "<tr>";
+                echo "<td>{$row[0]}</td>";
+                echo "<td>{$row[1]}</td>";
+                echo "<td>{$row[2]}</td>";
+                echo "<td>{$row[3]}</td>";
+                echo "</tr>";
+            }
+        }
+        ?>
         </tbody>
     </table>
 </div>
