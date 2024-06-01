0cadaf27ec4dbbc4a81814e3704de71f8b78fc20
GrigorevaAnna <72629044+GrigorevaAnna@users.noreply.github.com>
2024-06-01 16:40:25 +0200

    Connecting to mysql

--- Laba_5/code/index.php
+++ Laba_5/code/index.php
@@ -1,1 +1,8 @@
 <?php
+/* Подключение к серверу MySQL */
+$mysqli = new mysqli('db', 'root', 'helloworld', 'web');
+
+if (mysqli_connect_errno()) {
+    printf("Подключение к серверу MySQL невозможно. Код ошибки: %s\n", mysqli_connect_error());
+    exit;
+}
