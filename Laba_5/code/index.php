803a779470bea7724aa229ae6029c22d0302924b
GrigorevaAnna <72629044+GrigorevaAnna@users.noreply.github.com>
2024-06-01 16:45:27 +0200

    form processing

--- Laba_5/code/index.php
+++ Laba_5/code/index.php
@@ -6,3 +6,12 @@ if (mysqli_connect_errno()) {
     printf("Подключение к серверу MySQL невозможно. Код ошибки: %s\n", mysqli_connect_error());
     exit;
 }
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $email = $mysqli->real_escape_string($_POST['email']);
+    $title = $mysqli->real_escape_string($_POST['title']);
+    $category = $mysqli->real_escape_string($_POST['categories']);
+    $description = $mysqli->real_escape_string($_POST['text']);
+
+    $mysqli->query("INSERT INTO ad (email, title, description, category) VALUES ('$email', '$title', '$description', '$category')");
+}
